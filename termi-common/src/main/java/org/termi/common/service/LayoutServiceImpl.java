1e05c02560aa46b98361baf9bab2625d493bb792
Gary Meng <gary.meng3@gmail.com>
2023-01-03 16:06:09 +0400

    Fix No bean named 'xxxWidgetRenderer' available error

--- termi-common/src/main/java/org/termi/common/service/LayoutServiceImpl.java
+++ termi-common/src/main/java/org/termi/common/service/LayoutServiceImpl.java
@@ -1,11 +1,12 @@
 package org.termi.common.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.termi.common.entity.WidgetInstance;
 import org.termi.common.function.TriConsumer;
-import org.termi.common.repository.LayoutRepository;
 import org.termi.common.widget.WidgetPosition;
 import org.termi.common.widget.WidgetRender;
 
@@ -16,14 +17,12 @@ import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service("CommonLayoutService")
 public class LayoutServiceImpl implements LayoutService {
 
     @Autowired
     private ApplicationContext context;
-
-    @Autowired
-    private LayoutRepository repository;
 
     @Override
     public <T> Map<WidgetPosition, T> group(List<WidgetInstance> instances,
@@ -40,7 +39,15 @@ public class LayoutServiceImpl implements LayoutService {
 
         for (Map.Entry<WidgetPosition, List<WidgetInstance>> map : widgetGroup.entrySet()) {
             for (WidgetInstance widgetInstance : widgetGroup.get(map.getKey())) {
-                WidgetRender renderer = context.getBean(widgetInstance.getWidget().getRendererClassName(), WidgetRender.class);
+                String beanName = widgetInstance.getWidget().getRendererClassName();
+                WidgetRender renderer;
+                try {
+                    renderer = context.getBean(beanName, WidgetRender.class);
+                } catch (NoSuchBeanDefinitionException e) {
+                    log.error(e.getMessage());
+                    e.printStackTrace();
+                    continue;
+                }
 
                 T t = grouped.get(map.getKey());
                 consumer.accept(t, renderer, widgetInstance);
